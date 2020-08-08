199a68579392a37cf76b8df0881448f134622207
Bert <viniciusmvn@pm.me>
2020-08-07 23:41:57 -0300

    Finish web list and create proffys

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,8 +6,8 @@ import connection from './config/connection';
 const app = express();
 
 app.use(express.json());
-app.use(routes);
 app.use(cors());
+app.use(routes);
 
 connection.then(() => {
   app.listen(3333, () => console.log('Server Running on PORT 3333...'));
--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,34 +3,54 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  teacherId: {
+    id: string;
+    name: string;
+    avatar: string;
+    bio: string;
+    whatsapp: string
+  }
+  cost: number;
+  subject: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher,
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection(){
+    api.post('connections', {
+      teacherId: teacher.teacherId
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars0.githubusercontent.com/u/29619309?s=460&u=cdbf9ec14a4bf2b440f47089b44820f847c311e2&v=4"
-          alt="Marcos Vinícius" />
+        <img src={teacher.teacherId.avatar} alt={teacher.teacherId.name} />
         <div>
-          <strong>Marcos Vinícius</strong>
-          <span>Informática</span>
+          <strong>{teacher.teacherId.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Instrutor do laboratório de Informática
-          <br /><br />
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tristique, leo vitae pretium tristique, felis libero sollicitudin urna, sit amet blandit magna urna non nisi.
-        </p>
+      <p>{teacher.teacherId.bio}</p>
 
       <footer>
         <p>Preço/hora
-          <strong>R$ 50,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a target="_blank"
+        rel="noopener noreferrer"
+        onClick={createNewConnection} 
+        href={`https://wa.me/${teacher.teacherId.whatsapp}?text="Olá, ${teacher.teacherId.name}. Gostaria de ter aulas de ${teacher.subject}!`}>
           <img src={whatsappIcon} alt="whatsapp" />
             Entrar em contato
-          </button>
+          </a>
       </footer>
     </article>
   )
